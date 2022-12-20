f09f9bf055eee5cbaa27b04ee076a33cbd4d901b
Văn Gia Quyền <giaquyenvan@gmail.com>
2022-12-20 15:48:57 +0700

    first commit

--- Js/Index.js
+++ Js/Index.js
@@ -1,50 +1,53 @@
 //Bài 1: Tính tiền lương nhân viên
-//1. Input: Nhập Số ngày làm viêc ở đây
-let songay = 4;
-let wage = 1000000; //Lương nhân viên 1 ngày
-//2. Process
-let sum = wage * songay;
-//3. Output: Tính tiền lương nhân viên
-console.log("Bài tập 1: Lương nhân viên (VNĐ) = ", sum);
+
+document.getElementById("calculate").onclick = function () {
+    let wageEl = +document.getElementById("wage").value
+    let numberEl = +document.getElementById("number").value
+    document.getElementById('result1').value = new Intl.NumberFormat('vn-VN').format(wageEl * numberEl) + " VNĐ"
+    console.log("Bài tập 1: Lương nhân viên (VNĐ): ", new Intl.NumberFormat('vn-VN').format(wageEl * numberEl), "VNĐ")
+}
 
 //Bài 2: Tính giá trị trung bình của 5 số thực
-//1. Input: Nhập vào 5 số thực
-let a1 = 1; //Nhập số thứ nhất 
-let a2 = 2; //Nhập số thứ 2
-let a3 = 3; //Nhập số thứ 3
-let a4 = 4; //Nhập số thứ 4
-let a5 = 5; //Nhập số thứ 5
-//2. Process
-let average = (a1 + a2 + a3 + a4 + a5) / 5;
-//3. Output: Tính giá trị trung bình của 5 số
-console.log("Bài tập 2: Giá trị trung bình 5 số = ", average);
 
+document.getElementById("average").onclick = function () {
+    let number1El = +document.getElementById("number1").value
+    let number2El = +document.getElementById("number2").value
+    let number3El = +document.getElementById("number3").value
+    let number4El = +document.getElementById("number4").value
+    let number5El = +document.getElementById("number5").value
+    document.getElementById('result2').value = (number1El + number2El + number3El + number4El + number5El) / 5
+    console.log("Bài tập 2: Giá trị trung bình 5 số = ", (number1El + number2El + number3El + number4El + number5El) / 5)
+}
 //Bài 3: Quy đổi tiền
-//1. Input: Nhập vào số tiền USD
-let usd = 2300; //Nhập số tiền USD ở đây
-let k = 23500;  //Tỷ Gía USD
-//2. Process 
-let money = k * usd;
-//3. Output: Tính số tiền VNĐ quy đổi
-console.log("Bài tập 3: Số tiền VNĐ (đơn vị: VNĐ) = ", money);
+
+document.getElementById("tienVND").onclick = function () {
+    let usdEl = +document.getElementById("usd").value
+    document.getElementById('result3').value = new Intl.NumberFormat('vn-VN').format(usdEl * 23500) + " VNĐ"
+    console.log("Bài tập 3: Tiền VNĐ: ", new Intl.NumberFormat('vn-VN').format(usdEl * 23500))
+}
 
 //Bài 4: Tính diện tích, chu vi hình chữ nhật
-//1. Input: Nhập vào chiều dài, chiều rộng hình chữ nhật
-let x = 12; //chiều rộng hình chữ nhât
-let y = 28; //chiều dài hình chữ nhật
-//2. Process 
-let chuvi = 2 * (x + y);
-let dientich = x * y;
-console.log("Bài tập 4: Chu vi hình chữ nhật (đơn vị chu vi) = ", chuvi);
-console.log("Bài tập 4: Diện tích hình chữ nhật (đơn vị diện tích) = ", dientich);
+
+document.getElementById("perimeter").onclick = function () {
+    let widthEl = +document.getElementById("width").value
+    let lengthEl = +document.getElementById("length").value
+    document.getElementById('result41').value = 2 * (widthEl + lengthEl)
+    console.log("Bài tập 4: Chu vi: ", 2 * (widthEl + lengthEl), "(đvcv)")
+}
+
+document.getElementById("area").onclick = function () {
+    let widthEl = +document.getElementById("width").value
+    let lengthEl = +document.getElementById("length").value
+    document.getElementById('result42').value = widthEl * lengthEl
+    console.log("Bài tập 4: Diện tích: ", widthEl * lengthEl, "(đvdt)")
+}
 
 //Bài 5: Tính tổng 2 ký số
-//1. Input: Nhập vào số tự nhiên có 2 chữ số
-let number = 58; //Nhập số có 2 chữ số ở đây
-//2. Process
-u = number % 10;               //Lấy chữ số hàng đơn vị
-v = Math.floor(number / 10);   //Lấy chữ số hàng chục
-let Sum = u + v;
-//3. Output: Tính tổng 2 ký số
-console.log("Bài tập 5: Tổng 2 ký số = ", u + v);
 
+document.getElementById("sum").onclick = function () {
+    let naturalNumberEL = +document.getElementById("naturalNumber").value
+    let unitEl = naturalNumberEL % 10
+    let tenEl = Math.floor(naturalNumberEL / 10)
+    document.getElementById('result5').value = unitEl + tenEl
+    console.log("Bài tập 5: Tổng 2 ký số: ", unitEl + tenEl)
+}
